adb88d9a8aa1b343b1f624b8f6a8eac3b42d07c7
harshshelar22 <harsh.shelar@spit.ac.in>
2021-08-14 12:56:35 +0530

    Added seat grid

--- src/components/NewBooking.js
+++ src/components/NewBooking.js
@@ -17,6 +17,7 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import useNewBookingStyles from '../styles/NewBookingStyles';
 import Button from '@material-ui/core/Button';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import SeatGrid from './SeatGrid';
 
 const NewBooking = (props) => {
     const classes = useNewBookingStyles();
@@ -24,7 +25,7 @@ const NewBooking = (props) => {
     const [date, setDate] = useState(new Date('2021-08-18T21:11:54'));
     const [desk, setDesk] = useState(17);
     // const [image, setImage] = useState(null);
-    let floors = [1,2,3]
+    let floors = [1, 2, 3]
     let progress = 76
 
     const handleChange = (event) => {
@@ -32,7 +33,7 @@ const NewBooking = (props) => {
     };
 
     console.log(props);
-    
+
     useEffect(() => {
         console.log(date);
     }, [date])
@@ -41,7 +42,7 @@ const NewBooking = (props) => {
     //     import('../images/floor'+floor+'.jpg').then(image => setImage(image))
     // }, [floor])
 
-    return ( 
+    return (
         <div className={classes.root}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
@@ -49,14 +50,14 @@ const NewBooking = (props) => {
                         Booking Desk at {props.location.buildingName}
                     </Typography>
                 </Grid>
-                
+
                 <Grid item xs={12} md={4}>
                     <Box className={classes.paper}>
                         <Box className={classes.listChip}>
                             <Chip
                                 color="primary"
-                                label="Select and View Floor" 
-                                variant="outlined" 
+                                label="Select and View Floor"
+                                variant="outlined"
                                 className={classes.chip}
                             />
                         </Box>
@@ -89,21 +90,21 @@ const NewBooking = (props) => {
                                             onChange={handleChange}
                                             label="floor"
                                         >
-                                        {
-                                            floors.map((item, index) => (
-                                                <MenuItem value={item} key={index}>Floor {item}</MenuItem>
-                                            ))
-                                        }
+                                            {
+                                                floors.map((item, index) => (
+                                                    <MenuItem value={item} key={index}>Floor {item}</MenuItem>
+                                                ))
+                                            }
                                         </Select>
                                     </FormControl>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Box className={classes.progress}>
-                                        <LinearProgress 
-                                            variant="determinate" 
+                                        <LinearProgress
+                                            variant="determinate"
                                             value={progress}
-                                            color={progress > 75 ? 'secondary' : 'primary'} 
-                                        /> 
+                                            color={progress > 75 ? 'secondary' : 'primary'}
+                                        />
                                     </Box>
                                 </Grid>
                                 <Grid item xs={3} className={classes.percentage}>
@@ -114,7 +115,7 @@ const NewBooking = (props) => {
                     </Box>
                     <Box className={classes.image}>
                         <Zoom>
-                            <img src={image} alt={"floor"+floor} height="200" width="370" />
+                            <img src={image} alt={"floor" + floor} height="200" width="370" />
                         </Zoom>
                     </Box>
                     <Box className={classes.image}>
@@ -128,8 +129,8 @@ const NewBooking = (props) => {
                         <Box className={classes.listChip}>
                             <Chip
                                 color="primary"
-                                label="Select Date" 
-                                variant="outlined" 
+                                label="Select Date"
+                                variant="outlined"
                                 className={classes.chip}
                             />
                         </Box>
@@ -139,71 +140,71 @@ const NewBooking = (props) => {
                         <Box className={classes.listChip}>
                             <Chip
                                 color="primary"
-                                label="Select Desk" 
-                                variant="outlined" 
+                                label="Select Desk"
+                                variant="outlined"
                                 className={classes.chip}
                             />
                         </Box>
-                        Selection grid todo
+                        <SeatGrid />
                     </Box>
                 </Grid>
-                        <Divider orientation="vertical" flexItem />
+                <Divider orientation="vertical" flexItem />
                 {
                     date &&
-                        <Grid item xs={12} md={3}>
-                            <Box className={classes.paper}>
-                                <Box className={classes.listChip}>
-                                    <Chip
-                                        color="primary"
-                                        label="Booking Details" 
-                                        variant="outlined" 
-                                        className={classes.chip}
-                                    />
-                                </Box>
-                                
-                                <Box>
-                                    <Typography variant="h6">
-                                        LOCATION
-                                    </Typography>
-                                    {/* <Divider variant="inset"  /> */}
-                                    <Typography variant="overline" display="block" gutterBottom>
-                                        Yerawda
-                                    </Typography>
-                                    
-                                    <Typography variant="h6">
-                                        FLOOR
-                                    </Typography>
-                                    <Typography variant="overline" display="block" gutterBottom>
-                                        Floor {floor}
-                                    </Typography>
-                                    
-                                    <Typography variant="h6">
-                                        DATE
-                                    </Typography>
-                                    <Typography variant="overline" display="block" gutterBottom>
-                                        {date.getDate()} / {date.getMonth()} / {date.getFullYear()}
-                                    </Typography>
-                                    
-                                    <Typography variant="h6">
-                                        Desk
-                                    </Typography>
-                                    <Typography variant="overline" display="block" gutterBottom>
-                                        28
-                                    </Typography>
-                                </Box>
-
-                                <Box>
-                                    <Button variant="contained" className={classes.confirmButton}>
-                                        <CheckCircleOutlineIcon /> &nbsp;
-                                         Confirm Booking
-                                    </Button>
-                                </Box>
+                    <Grid item xs={12} md={3}>
+                        <Box className={classes.paper}>
+                            <Box className={classes.listChip}>
+                                <Chip
+                                    color="primary"
+                                    label="Booking Details"
+                                    variant="outlined"
+                                    className={classes.chip}
+                                />
                             </Box>
-                        </Grid>
+
+                            <Box>
+                                <Typography variant="h6">
+                                    LOCATION
+                                </Typography>
+                                {/* <Divider variant="inset"  /> */}
+                                <Typography variant="overline" display="block" gutterBottom>
+                                    Yerawda
+                                </Typography>
+
+                                <Typography variant="h6">
+                                    FLOOR
+                                </Typography>
+                                <Typography variant="overline" display="block" gutterBottom>
+                                    Floor {floor}
+                                </Typography>
+
+                                <Typography variant="h6">
+                                    DATE
+                                </Typography>
+                                <Typography variant="overline" display="block" gutterBottom>
+                                    {date.getDate()} / {date.getMonth()} / {date.getFullYear()}
+                                </Typography>
+
+                                <Typography variant="h6">
+                                    Desk
+                                </Typography>
+                                <Typography variant="overline" display="block" gutterBottom>
+                                    28
+                                </Typography>
+                            </Box>
+
+                            <Box>
+                                <Button variant="contained" className={classes.confirmButton}>
+                                    <CheckCircleOutlineIcon /> &nbsp;
+                                    Confirm Booking
+                                </Button>
+                            </Box>
+                        </Box>
+                    </Grid>
                 }
-                </Grid>
-            </div>
-     );
+            </Grid>
+        </div>
+    );
 }
- 
+
 export default NewBooking;
---
+++ src/components/SeatGrid.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import db_desk_normal from '../images/db_office.png';
+import db_desk_red from '../images/db_desk_red.png';
+import db_desk_green from '../images/db_desk_green.png';
+import db_desk_muted from '../images/db_desk_muted.png'
+
+
+function SeatGrid() {
+
+    const arr = [1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6]
+
+    return (
+        <div style={{
+            marginTop: "2em",
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            flexWrap: "wrap"
+        }}>
+
+            {
+                arr.map((item, idx) => {
+                    if (item < 3) {
+                        return (
+                            <div style={{
+                                margin: "0.5em"
+                            }}>
+                                <img src={db_desk_red} alt="desk" style={{ height: 32, width: 32 }} />   
+                                <p style={{margin: 0, padding: 0}}>{idx + 1}</p>
+                            </div>
+                        );
+                    } else if (item === 3) {
+                        return (
+                            <div style={{
+                                margin: "0.5em"
+                            }}>
+                                <img src={db_desk_green} alt="desk" style={{ height: 32, width: 32 }} />   
+                                <p style={{margin: 0, padding: 0}}>{idx + 1}</p>
+                            </div>
+                        );
+                    } else if (item === 4){
+                        return (
+                            <div style={{
+                                margin: "0.5em"
+                            }}>
+                                <img src={db_desk_muted} alt="desk" style={{ height: 32, width: 32 }} />   
+                                <p style={{margin: 0, padding: 0}}>{idx + 1}</p>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div style={{
+                                margin: "0.5em"
+                            }}>
+                                <img src={db_desk_normal} alt="desk" style={{ height: 32, width: 32 }} />   
+                                <p style={{margin: 0, padding: 0}}>{idx + 1}</p>
+                            </div>
+                        );
+                    }
+
+                })
+            }
+        </div>
+    );
+}
+
+export default SeatGrid
