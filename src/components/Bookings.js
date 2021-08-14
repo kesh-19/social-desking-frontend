15636c36a60b16697f86e6de2de2a33bb959602e
Dushyant Pathak <dushyantpathak6789@gmail.com>
2021-08-14 14:36:47 +0530

    new design bookings

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -2,15 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
 import { Button, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
 import bg from '../images/DB2.jpg';
 import BookingCard from './BookingCard';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 345,
-        flexGrow: 1,
-        margin: "1em 0",
+        flexGrow: 1
     },
     media: {
         height: 140,
@@ -24,7 +23,7 @@ const Bookings = () => {
     
     
     const classes = useStyles();
-    const dataUrl = 'http://localhost:8000'
+    const dataUrl = 'https://8000-white-gibbon-p4qhs88b.ws-us14.gitpod.io'
 
     const [bookings, setBookings] = useState([])
     
@@ -35,9 +34,21 @@ const Bookings = () => {
     }, [])
 
     return (
-        bookings.map(booking => (
-            <BookingCard booking={booking} key={booking.userid} />
-        ))
+        <div className={classes.root}>
+            <Grid
+                    container
+                    justifyContent="flex-start"
+                    alignItems="center">
+                { 
+                    bookings.map(booking => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <BookingCard booking={booking} key={booking.userid} />
+                        </Grid>
+                    ))
+                }
+                </Grid>
+        </div>
+        
     )
 }
 
