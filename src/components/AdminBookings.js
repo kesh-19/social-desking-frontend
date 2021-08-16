c653983890fc4ea17e70ffebefe47324d2438326
dkp1903 <dushyantpathak6789@gmail.com>
2021-08-16 07:19:30 +0000

    functional

--- src/components/AdminBookings.js
+++ src/components/AdminBookings.js
@@ -7,56 +7,87 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { DataGrid } from '@material-ui/data-grid';
 
+import Config from '../Config'
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(id, bookingid, seatid, buildingid, floorno, dob) {
+  return { id, bookingid, seatid, buildingid, floorno, dob };
 }
 
-const rows = [
-  createData(1, 1, 1, 1, '190201223'),
-  createData(1, 1, 1, 1, '190201223'),
-  createData(1, 1, 1, 1, '190201223'),
-  createData(1, 1, 1, 1, '190201223'),
-  createData(1, 1, 1, 1, '190201223')
+
+const columns = [
+  { 
+    field: 'id', 
+    headerName: 'Emp ID', 
+    width: 150 
+  },
+  {
+    field: 'bookingid',
+    headerName: 'Book',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'seatid',
+    headerName: 'Seat',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'buildingid',
+    headerName: 'Buil ID',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'floorno',
+    headerName: 'Flr',
+    width: 160,
+    
+  },
+  {
+    field: 'dob',
+    headerName: 'Date of booking',
+    width: 160,
+    
+  },
 ];
 
 const AdminBookings = () => {
   const classes = useStyles();
+  const dataUrl = `${Config.serverUrl}/desking/booking/alldetails`
+  const [allBookings, setAllBookings] = useState([])
+
+  useEffect(() => {
+    fetch(dataUrl)
+      .then(res => res.json())
+      .then(data => {
+        console.log("Allbookings: ", data)
+        const rows = data.map((booking) => {
+          return createData(booking[0], booking[3], booking[5], booking[7], booking[6], booking[4])
+        })
+        setAllBookings(rows)
+      })
+  }, [])
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Employee ID</TableCell>
-            <TableCell align="right">Building ID</TableCell>
-            <TableCell align="right">Floor Number</TableCell>
-            <TableCell align="right">SeatID</TableCell>
-            <TableCell align="right">Date of reservation</TableCell>
-            
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={allBookings}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
   );
 }
 
